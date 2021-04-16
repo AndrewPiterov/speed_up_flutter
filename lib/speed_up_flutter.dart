165d2daa1a4a55b569354f16bcbd11b51022fcfb
Andrew Piterov <piterov1990@gmail.com>
2021-04-16 17:22:35 +0200

    [Add] example project

--- lib/speed_up_flutter.dart
+++ lib/speed_up_flutter.dart
@@ -3,5 +3,5 @@ library speed_up_flutter;
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+  static int addOne(int value) => value + 1;
 }
